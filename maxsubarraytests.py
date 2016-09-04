b36cd2f06c10473ddacc5cea5b7690de3a8848d5
Mark Tse <neverendingqs@users.noreply.github.com>
2016-09-04 15:27:54 -0400

    Cleaning up test file. (#2)

--- maxsubarraytests.py
+++ maxsubarraytests.py
@@ -12,40 +12,41 @@ class maxsubarrayTests(unittest.TestCase):
     
     def setUp(self):
         self.array = maxsubarray()
-        self.testcases = []
-        
-        self.testcases.append({'input':[-2, 1, -3, 4, -1, 2, 1, -5, 4], 'index':3, 'length':4, 'sum':6})
-        self.testcases.append({'input':[1, 2, 3, -7, 8, -1, 8], 'index':4, 'length':3, 'sum':15})
-        self.testcases.append({'input':[1, 2, 3, -5, 8, -1, 8], 'index':0, 'length':7, 'sum':16})
-        self.testcases.append({'input':[], 'index':0, 'length':0, 'sum':0})
-        self.testcases.append({'input':[-5, -9, 0, -20], 'index':0, 'length':0, 'sum':0})
-        self.testcases.append({'input':[-5, -9, -20], 'index':0, 'length':0, 'sum':0})
-    
+        self.testcases = [
+            {'input':[-2, 1, -3, 4, -1, 2, 1, -5, 4], 'index':3, 'length':4, 'sum':6},
+            {'input':[1, 2, 3, -7, 8, -1, 8], 'index':4, 'length':3, 'sum':15},
+            {'input':[1, 2, 3, -5, 8, -1, 8], 'index':0, 'length':7, 'sum':16},
+            {'input':[], 'index':0, 'length':0, 'sum':0},
+            {'input':[-5, -9, 0, -20], 'index':0, 'length':0, 'sum':0},
+            {'input':[-5, -9, -20], 'index':0, 'length':0, 'sum':0}
+        ]
+
         pass
 
     def tearDown(self):
         pass
 
 
-    def testKadane(self):
-        for i in range(len(self.testcases)):
-            self.assertEqual(self.array.kadane(self.testcases[i]['input']), self.testcases[i]['sum'])
-        pass
+    def test_kadane(self):
+        for test_case in self.testcases:
+            with self.subTest(test_case=test_case):
+                expected_results = test_case['sum']
+                actual_results = self.array.kadane(test_case['input'])
+                self.assertEqual(expected_results, actual_results)
 
-    def testKadaneWithSubarray(self):
-        for i in range(len(self.testcases)):
-            expectedResults = self.testcases[i]['index'], self.testcases[i]['length'], self.testcases[i]['sum']
-            actualResults = self.array.kadaneWithSubarray(self.testcases[i]['input'])
-            self.assertEqual(expectedResults, actualResults)
-        pass
+    def test_kadane_with_subarray(self):
+        for test_case in self.testcases:
+            with self.subTest(test_case=test_case):
+                expected_results = test_case['index'], test_case['length'], test_case['sum']
+                actual_results = self.array.kadaneWithSubarray(test_case['input'])
+                self.assertEqual(expected_results, actual_results)
     
-    def testBruteForce(self):
-        for i in range(len(self.testcases)):
-            expectedResults = self.testcases[i]['index'], self.testcases[i]['length'], self.testcases[i]['sum']
-            actualResults = self.array.bruteForce(self.testcases[i]['input'])
-            self.assertEqual(expectedResults, actualResults)
-        pass
+    def test_brute_force(self):
+        for test_case in self.testcases:
+            with self.subTest(test_case=test_case):
+                expected_results = test_case['index'], test_case['length'], test_case['sum']
+                actual_results = self.array.bruteForce(test_case['input'])
+                self.assertEqual(expected_results, actual_results)
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
