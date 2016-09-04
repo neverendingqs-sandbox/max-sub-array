30703e1faa6c9a34c2b28b1fedd5845aba564c14
Mark Tse <mark.tse@neverendingqs.com>
2016-09-04 15:28:55 -0400

    Removing unneeded class usage.

--- maxsubarraytests.py
+++ maxsubarraytests.py
@@ -6,12 +6,11 @@ Uses the unittest library to run tests on different implementations of the maxsu
 '''
 
 import unittest
-from maxsubarray import maxsubarray
+import maxsubarray
 
-class maxsubarrayTests(unittest.TestCase):
+class MaxsubarrayTests(unittest.TestCase):
     
     def setUp(self):
-        self.array = maxsubarray()
         self.testcases = [
             {'input':[-2, 1, -3, 4, -1, 2, 1, -5, 4], 'index':3, 'length':4, 'sum':6},
             {'input':[1, 2, 3, -7, 8, -1, 8], 'index':4, 'length':3, 'sum':15},
@@ -21,31 +20,25 @@ class maxsubarrayTests(unittest.TestCase):
             {'input':[-5, -9, -20], 'index':0, 'length':0, 'sum':0}
         ]
 
-        pass
-
-    def tearDown(self):
-        pass
-
-
     def test_kadane(self):
         for test_case in self.testcases:
             with self.subTest(test_case=test_case):
                 expected_results = test_case['sum']
-                actual_results = self.array.kadane(test_case['input'])
+                actual_results = maxsubarray.kadane(test_case['input'])
                 self.assertEqual(expected_results, actual_results)
 
     def test_kadane_with_subarray(self):
         for test_case in self.testcases:
             with self.subTest(test_case=test_case):
                 expected_results = test_case['index'], test_case['length'], test_case['sum']
-                actual_results = self.array.kadaneWithSubarray(test_case['input'])
+                actual_results = maxsubarray.kadaneWithSubarray(test_case['input'])
                 self.assertEqual(expected_results, actual_results)
     
     def test_brute_force(self):
         for test_case in self.testcases:
             with self.subTest(test_case=test_case):
                 expected_results = test_case['index'], test_case['length'], test_case['sum']
-                actual_results = self.array.bruteForce(test_case['input'])
+                actual_results = maxsubarray.bruteForce(test_case['input'])
                 self.assertEqual(expected_results, actual_results)
 
 if __name__ == "__main__":
